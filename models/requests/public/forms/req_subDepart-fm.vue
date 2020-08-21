ddd093d417c8b834977a9f41fd164025039d60f5
alex5272372 <alex5272372@gmail.com>
2020-08-21 16:56:07 +0300

    Vue form begin

---
+++ models/requests/public/forms/req_subDepart-fm.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-form label-position="left" label-width="180px" class="req_subDepartment__form">
+  </el-form>
+</template>
+
+<script>
+module.exports.default = {}
+
+const { mountUtils, Form } = require("@unitybase/adminui-vue")
+module.exports.mount = function({ title, entity, instanceID, formCode, rootComponent }) {
+  // mountUtils.mountModal
+  Form({ component: rootComponent, title, formCode, entity: entity }).mount()
+}
+</script>
+
+<style>
+.req_subDepartment__form {
+  overflow: auto;
+  height: 100%;
+}
+</style>
