956575fbb73a690a485dc99f6a41d842d26b2c66
alex5272372 <alex5272372@gmail.com>
2020-08-31 19:34:47 +0300

    Own server method

---
+++ models/requests/public/forms/req_depart-fm.js
@@ -0,0 +1,38 @@
+/* global UB $App Ext */
+
+exports.formCode = {
+  addBaseActions: function () {
+    this.callParent(arguments)
+    this.actions.ActionReassignedAll = new Ext.Action({
+      actionId: 'ActionReassignedAll',
+      actionText: UB.i18n('Reassigned all requests'),
+      handler: reAssignAll.bind(this)
+    })
+  },
+  initUBComponent: function () {
+    var me = this
+    me.getField('ID').readOnly = true
+  }
+}
+function reAssignAll () {
+  var form = this
+  $App.showModal({
+    formCode: 'req_reqList-reassignAll',
+    description: UB.i18n('Reassign to')
+  }).then(function (result) {
+    if (result.action === 'ok') {
+      if (result.newDep !== form.getField('ID').getValue()) {
+        $App.connection.query({
+          entity: 'req_depart',
+          method: 'reassignDep',
+          curDep: form.getField('ID').getValue(),
+          newDep: result.newDep
+        }).then(function () {
+          $App.dialogInfo('Requests Reassigned Successfully')
+        })
+      } else {
+        $App.dialogError('Departments must be different')
+      }
+    }
+  })
+}
--- models/requests/req_depart.js
+++ models/requests/req_depart.js
@@ -1,7 +1,45 @@
-const __entityName = __filename.slice(__dirname.length + 1, -3)
-const me = global[__entityName]
+const UB = require('@unitybase/ub')
+const me = global.req_depart
+me.entity.addMethod('reassignDep')
 
-me.on('insert:before', beforeInsert)
+/**
+ * Reassign all departments form params.curDep to params.newDep
+ * @param {ubMethodParams} ctxt
+ */
+function reassignDep (ctxt) {
+  'use strict'
+  var params = ctxt.mParams
+  var curDep = params.curDep
+  var newDep = params.newDep
 
-function beforeInsert (ctx) {
+  if (!curDep) {
+    throw new Error('curDep parameter is required')
+  }
+  if (!newDep) {
+    throw new Error('newDep parameter is required')
+  }
+  if (curDep === newDep) {
+    throw new Error('curDep and newDep parameters must be different')
+  }
+  var store = UB.DataStore('req_reqList')
+  var updStore = UB.DataStore('req_reqList')
+  UB.Repository('req_reqList')
+    .attrs('ID', 'department', 'subDepartment', 'mi_modifyDate')
+    .where('department', '=', curDep)
+    .select(store)
+
+  while (!store.eof) {
+    updStore.run('update', {
+      entity: 'req_reqList',
+      lockType: 'Temp',
+      execParams: {
+        department: newDep,
+        subDepartment: null,
+        ID: store.get('ID'),
+        mi_modifyDate: store.get('mi_modifyDate')
+      }
+    })
+    store.next()
+  }
 }
+me.reassignDep = reassignDep
